3fec652f3995b728a417730622f32785ff5d2d6f
ashtonsprunger <sprungerashton@gmail.com>
2021-01-27 14:52:36 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import queryString from "querystring";
+import io from "socket.io-client";
+
+let socket;
+
+const App = () => {
+  // const socketRef = useRef();
+  const [count, setCount] = useState();
+
+  useEffect(() => {
+    socket = io("localhost:3333");
+    // socket.emit("here");
+    console.log("SOCKET", socket);
+
+    socket.on("count", (count) => {
+      setCount(count);
+    });
+  }, []);
+
+  return <div className="App">{count}</div>;
+};
+
+export default App;
